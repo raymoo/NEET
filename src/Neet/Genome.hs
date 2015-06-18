6a88cd80bf0de2af8de4887da7031dee1463b612
raymoo <uguu@installgentoo.com>
2015-06-18 12:35:37 -0700

    Genome

---
+++ src/Neet/Genome.hs
@@ -0,0 +1,112 @@
+{-
+Copyright (C) 2015 Leon Medvinsky
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-}
+
+{-|
+Module      : Neet.Genome
+Description : Encodings NEAT genomes
+Copyright   : (c) Leon Medvinsky, 2015
+
+License     : GPL-3
+Maintainer  : lmedvinsky@hotmail.com
+Stability   : experimental
+Portability : portable
+-}
+
+{-# LANGUAGE RecordWildCards #-}
+
+module Neet.Genome ( -- * Genes
+                     NodeId(..)
+                   , NodeType(..)
+                   , NodeGene(..)
+                   , ConnGene(..)
+                   , InnoId(..)
+                     -- * Genome
+                   , Genome(..)
+                     -- ** Construction
+                   , fullConn
+                   ) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Random.Class
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+
+
+-- | The IDs node genes use to refer to nodes.
+newtype NodeId = NodeId Int
+               deriving (Show, Eq, Ord)
+
+
+-- | Types of nodes
+data NodeType = Input | Hidden | Output
+              deriving (Show)
+
+
+-- | Node genes
+data NodeGene = NodeGene { nodeType :: NodeType
+                         , yHint :: Rational -- ^ A hint for recurrency
+                         }
+              deriving (Show)
+
+
+-- | Connection genes
+data ConnGene = ConnGene { connIn :: NodeId
+                         , connOut :: NodeId
+                         , connWeight :: Double
+                         , connEnabled :: Bool
+                         , connRec :: Bool -- ^ A hint for recurrency
+                         }
+              deriving (Show)
+
+
+-- | Innovation IDs
+newtype InnoId = InnoId Int
+               deriving (Show, Eq, Ord)
+
+
+-- | A NEAT genome. The innovation numbers are stored in here, and not the genes,
+-- to prevent data duplication.
+data Genome =
+  Genome { nodeGenes :: Map NodeId NodeGene
+         , connGenes :: Map InnoId ConnGene
+         , nextNode :: NodeId
+         }
+  deriving (Show)
+
+
+-- | Takes the number of inputs, the number of outputs, and gives a genome with
+-- the inputs fully connected to the outputs with random weights. The order of
+-- the connections are deterministic, so when generating a population, you
+-- can just start the innovation number at iSize * oSize + 1
+fullConn :: MonadRandom m => Int -> Int -> m Genome
+fullConn iSize oSize = do
+  let inIDs = map NodeId [1..iSize]
+      outIDs = map NodeId [iSize + 1..oSize + iSize]
+      inputGenes = zip inIDs $ repeat (NodeGene Input 0)
+      outputGenes = zip outIDs $ repeat (NodeGene Output 1)
+      nodeGenes = M.fromList $ inputGenes ++ outputGenes
+      nextNode = NodeId $ iSize + oSize + 1
+      nodePairs = (,) <$> inIDs <*> outIDs
+  conns <- zipWith (\(inN, outN) w -> ConnGene inN outN w True False) nodePairs `liftM` getRandomRs (-1,1)
+  let connGenes = M.fromList $ zip (map InnoId [1..iSize * oSize]) conns
+  return $ Genome{..}
+
+
+mutateWeights :: MonadRandom m => Double -> Genome -> m Genome
+mutateWeights = undefined

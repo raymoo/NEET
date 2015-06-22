70172eb4b6b34ee61099078b61e7aa958bc6ceb4
raymoo <uguu@installgentoo.com>
2015-06-21 23:51:45 -0700

    Cut out the middleman

--- src/Neet/Genome.hs
+++ src/Neet/Genome.hs
@@ -342,8 +342,8 @@ superLeft comb mk = IM.mergeWithKey (\_ a b -> Just $ comb a b) (IM.map mk) (con
 -- | Choose between two alternatives with coin chance
 flipCoin :: MonadRandom m => a -> a -> m a
 flipCoin a1 a2 = do
-  roll <- (`mod` (2 :: Int)) `liftM` getRandom
-  return $ if roll == 0 then a1 else a2
+  roll <- getRandom
+  return $ if roll then a1 else a2
 
 
 -- | Crossover on just the connections. Put the fittest map first.
